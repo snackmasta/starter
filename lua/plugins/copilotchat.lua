4aedf1bc26485c0d7af946d59845cf4f7aace1b3
snackmasta <agungrambujana@gmail.com>
2025-06-27 01:48:34 +0700

    fix: CopilotChat

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,12 +1,14 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
+  lazy = false, -- Load it immediately, or set event = "InsertEnter" if you want lazy loading
   dependencies = {
     { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
     { "nvim-lua/plenary.nvim", branch = "master" },
   },
   build = "make tiktoken",
-  opts = {
-    -- Optional CopilotChat options go here
-  },
-  -- cmd = { "CopilotChat", "CopilotChatToggle" },
+  opts = {},
+  cmd = { "CopilotChat", "CopilotChatToggle" },
+  config = function(_, opts)
+    require("CopilotChat").setup(opts or {})
+  end,
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,21 @@ return {
     "github/copilot.vim",
     lazy = false, -- Load it immediately, or set event = "InsertEnter" if you want lazy loading
   },
-
+  -- CopilotChat 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = false, -- Load it immediately, or set event = "InsertEnter" if you want lazy loading
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {},
+    cmd = { "CopilotChat", "CopilotChatToggle" },
+    config = function(_, opts)
+      require("CopilotChat").setup(opts or {})
+    end,
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
